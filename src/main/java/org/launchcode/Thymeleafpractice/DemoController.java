19c6e2fefd4012cdeb0df7913341ac301941362b
AmberBaumann <ambermindsolliving@gmail.com>
2021-02-24 17:08:21 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/Thymeleafpractice/DemoController.java
@@ -0,0 +1,10 @@
+package org.launchcode.Thymeleafpractice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+
+    @RequestMapping
+}
